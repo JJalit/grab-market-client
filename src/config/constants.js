304abc3f54286ae39c69f91e9050b0f939400ee1
JaeHa <jaeha23@naver.com>
2021-01-28 10:09:49 +0900

    lets

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://jaeha-market-server.herokuapp.com"
+    ? "https://jaeha-grab-server.herokuapp.com"
     : "http://localhost:8080";
